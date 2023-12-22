68507244a095e053386573f8dcb7c1992ce69afc
Ajirin01 <mubarakolagoke@gmail.com>
2023-12-23 00:17:30 +0100

    add file added

---
+++ app/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'socialpack_db');
+define('TIMEZONE', 'America/Kentucky/Louisville');
+define('ENCRYPTION_KEY', '6752e0d9d65634be20b8f32305093470');
